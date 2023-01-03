ade3feb79a32b21e7f6c518c5b7141015cd6628c
Ievgen Babenko <evgbabenko@live.com>
2023-01-03 13:56:00 +0200

    hooks correction

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,7 +2,7 @@ import { signInAnonymously } from 'firebase/auth';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import useAuth from '../hooks/useAuth';
 
 interface Inputs {
