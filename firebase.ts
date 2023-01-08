c1d91723126001f4051da2589f0185acafc24f20
Ievgen Babenko <evgbabenko@live.com>
2023-01-08 10:10:44 +0200

    google api key

--- firebase.ts
+++ firebase.ts
@@ -10,7 +10,7 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${process.env.NEXT_GOOGLE_API_KEY}`,
+  apiKey: 'AIzaSyCGJnjDTaM7zDyH3dBbRuDTaI8L_0r80OE',
   authDomain: 'moovie-e184a.firebaseapp.com',
   projectId: 'moovie-e184a',
   storageBucket: 'moovie-e184a.appspot.com',
